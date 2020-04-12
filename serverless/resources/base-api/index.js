797a99814fa7f595b25261117822b165785a40a5
Jarryd Deiss <deiss.jad@gmail.com>
2020-04-12 16:54:45 +0200

    Added sample serverless assets

---
+++ serverless/resources/base-api/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable */
+// Dummy function for deployment of the base api which is used to ensure all apis are linked to the same api endpoint
+exports.handler = async function (event, context) {}
